6c7acbf30d6e15895bde3c7a13b1be1e0d8f3a16
SeungukLee <seunguk@seunguk.dev>
2020-10-22 15:34:10 +0900

    add starship config

--- bootstrap.sh
+++ bootstrap.sh
@@ -90,6 +90,12 @@ source $HOME/.zshrc
 mkdir -p $HOME/.gnupg && cp $DOTFILES/gpg-agent.conf ~/.gnupg
 
 #-------------------------------------------------------------------------------
+# Starship config
+#-------------------------------------------------------------------------------
+
+ln -sfn $DOTFILES/starship.toml $HOME/.config/starship.toml
+
+#-------------------------------------------------------------------------------
 # Set OS X preferences
 # We will run this last because this will reload the shell
 # Fix backtick(`) issue @see https://ani2life.com/wp/?p=1753
