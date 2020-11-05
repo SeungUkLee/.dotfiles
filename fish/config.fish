dd75c2bca98790c309ab395aa52a913e675636f5
SeungukLee <seunguk@seunguk.dev>
2020-11-05 13:07:05 +0900

    add: fish shell config

---
+++ fish/config.fish
@@ -0,0 +1,33 @@
+set -gx GPG_TTY (tty)
+
+source ~/.config/fish/aliases.fish
+source ~/.config/fish/path.fish
+source ~/.config/fish/export.fish
+source ~/.config/fish/functions.fish
+
+#-------------------------------------------------------------------------------
+# Path to your dotfiles installation.
+#-------------------------------------------------------------------------------
+
+set -x DOTFILES $HOME/.dotfiles
+
+#-------------------------------------------------------------------------------
+# starship config
+#-------------------------------------------------------------------------------
+
+starship init fish | source
+
+#-------------------------------------------------------------------------------
+# zoxide config
+#-------------------------------------------------------------------------------
+
+zoxide init fish | source
+
+#-------------------------------------------------------------------------------
+# asdf config
+# "echo -e "\nsource "(brew --prefix asdf)"/asdf.fish" >> ~/.config/fish/config.fish" excution result
+# @see https://asdf-vm.com/#/core-manage-asdf?id=add-to-your-shell
+#-------------------------------------------------------------------------------
+
+source /usr/local/opt/asdf/asdf.fish
+
---
+++ fish/path.fish
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------------------------
+# brew doctor
+# Warning: Homebrew's sbin was not found in your PATH but you have installed
+# formulae that put executables in /usr/local/sbin.
+# Consider setting the PATH for example like so
+#-------------------------------------------------------------------------------
+
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+
+
