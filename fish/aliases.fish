1dc0f4516c3f1002c9f677ff999e3d0d78fdcaad
SeungukLee <seunguk@seunguk.dev>
2024-01-03 06:57:14 +0900

    change to `eza` because the `exa` command has been deprecated.

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -7,10 +7,15 @@ abbr -a cls clear
 abbr -a til 'code $HOME/Documents/Github/TIL'
 abbr -a ex exercism
 
-# alias vim 'nvim'
-alias vi nvim
+if type -q eza
+    alias ll "eza -l -g --icons"
+    alias lla "ll -a"
+    abbr -a tree "eza -TL"
+end
+
+# alias vi nvim
+alias vim nvim
 alias cat 'bat --theme=(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo default || echo GitHub)'
-alias ls 'exa -h --icons'
 alias ghpv 'gh issue list | fzf | awk '\''{print $1}'\'' | xargs gh issue view'
 alias ghiv 'gh pr list | fzf | awk '\''{print $1}'\'' | xargs gh pr view'
 alias mv 'mv -i'
